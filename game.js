3503894fbc4a84ad53f2df9595599351cf11ee8e
nassetti <nassettimax@gmail.com>
2022-02-19 15:53:14 +0000

    adding JS

---
+++ game.js
@@ -0,0 +1,11 @@
+let correctOrder = [];
+let humanOrder =[];
+
+// adding arrowkey variables
+let up = document.getElementById("up")
+let right = document.getElementById("right")
+let down = document.getElementById("down")
+let left = document.getElementById("left")
+
+// adding start functionality
+
--- index.js
+++
@@ -1,2 +0,0 @@
-let correctOrder = [];
-let humanOrder =[];
