57d651764bfa3d9a307d67de299105748e432b99
nassetti <nassettimax@gmail.com>
2022-02-20 22:23:01 +0000

    Updates

--- js/game.js
+++ js/game.js
@@ -1,11 +1,27 @@
 let correctOrder = [];
-let humanOrder =[];
+let humanOrder = [];
 
 // adding arrowkey variables on screen
-let up = document.getElementById("up")
-let right = document.getElementById("right")
-let down = document.getElementById("down")
-let left = document.getElementById("left")
+let up = document.getElementById("up");
+let right = document.getElementById("right");
+let down = document.getElementById("down");
+let left = document.getElementById("left");
+let upBtn = document.getElementById("up-button");
+let rightBtn = document.getElementById("right-button");
+let downBtn = document.getElementById("down-button");
+let leftBtm = document.getElementById("left-button");
 
+// testing link
+upBtn.addEventListener("click", () => {
+  console.log("clicked question");
+});
+leftBtn.addEventListener("click", () => {
+  console.log("clicked question");
+});
+downBtn.addEventListener("click", () => {
+  console.log("clicked question");
+});
+rightBtn.addEventListener("click", () => {
+  console.log("clicked question");
+});
 // adding start functionality
-
