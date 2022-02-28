917805c72ddc7d9814d0253c027756e283e05426
nassetti <nassettimax@gmail.com>
2022-02-28 19:53:10 +0000

    JS Update

--- js/game.js
+++ js/game.js
@@ -6,13 +6,22 @@ let up = document.getElementById("up");
 let right = document.getElementById("right");
 let down = document.getElementById("down");
 let left = document.getElementById("left");
+
 let upBtn = document.getElementById("up-button");
 let rightBtn = document.getElementById("right-button");
 let downBtn = document.getElementById("down-button");
 let leftBtn = document.getElementById("left-button");
 
+// arrow key keyboard link
+let upArrowUser = document.getElementById("up-button");
+let rightArrowUser = document.getElementById("right-button");
+let downArrowUser = document.getElementById("down-button");
+let leftArrowUser = document.getElementById("left-button");
+
+
 // testing link
 upBtn.addEventListener("click", () => {
+  up.classList.add("active-red");  
   console.log("clicked up");
 });
 leftBtn.addEventListener("click", () => {
@@ -24,4 +33,7 @@ downBtn.addEventListener("click", () => {
 rightBtn.addEventListener("click", () => {
   console.log("clicked right");
 });
+
+// testing arrowkey link
+
 // adding start functionality
