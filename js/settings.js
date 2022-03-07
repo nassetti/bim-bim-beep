2ac1062bd560f1bfa8b3dad0eb21aba53a569d59
nassetti <nassettimax@gmail.com>
2022-03-07 10:46:47 +0000

    adding muted/unmuted functionality

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,7 @@
 // Adapted from code from: https://www.w3schools.com/howto/howto_css_modals.asp
 console.log("i am connected");
 let soundBtn = document.getElementById("bell");
-let soundIcon = document.getElementById("bell-icon")
+let mutedBtn = document.getElementById("bell-muted")
 let gearBtn = document.getElementById("gear");
 let tutorialBtn = document.getElementById("tutBtn");
 
@@ -11,6 +11,13 @@ let closeTut = document.getElementById("close-tut");
 let closeSetting = document.getElementById("close-setting");
 let modals = document.getElementsByClassName("modal")[0];
 
+// linking sounds
+var upSound = document.querySelector("#up-sound");
+var downSound = document.querySelector("#down-sound");
+var leftSound = document.querySelector("#left-sound");
+var rightSound = document.querySelector("#up-sound");
+
+// Modal set up
 tutorialBtn.addEventListener("click", () => {
     tutorialModal.style.display = "block";
     console.log("clicked question");
@@ -32,20 +39,24 @@ closeSetting.addEventListener("click", () => {
   console.log("clicked X on setting")
 });
 
-window.onclick = function (event) {
-  if (event.target == modals) {
-    modals.style.display = "none";
-    console.log("clicked outside")
-  }
-};
+// sound settings
+// turn sound off 
+soundBtn.addEventListener("click", () => {
+  mutedBtn.classList.remove("hidden");
+  soundBtn.classList.add("hidden");
+  upSound.muted = true;
+  leftSound.muted = true;
+  downSound.muted = true;
+  rightSound.muted = true;
 
-if (soundIcon.classList.contains("translucent") = true) {
-  soundBtn.addEventListener("click", () => {
-    soundIcon.classList.remove("translucent");
-  })
-}
-else {
-  soundBtn.addEventListener("click", () => {
-    soundIcon.classList.add("translucent");
-  })
-}
+});
+// turn sound on
+mutedBtn.addEventListener("click", () => {
+  soundBtn.classList.remove("hidden");
+  mutedBtn.classList.add("hidden");
+  upSound.muted = false;
+  leftSound.muted = false;
+  downSound.muted = false;
+  rightSound.muted = false;
+});
+
